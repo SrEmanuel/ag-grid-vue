de442cf39a61d9d8826b06c5fe8b3e62b04d0bf7
Emanuel Martins <emanuelmartins284@gmail.com>
2023-03-22 07:53:44 -0300

    uploading files

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+   <ag-grid-vue
+        style="width: 35rem; height: 25rem;"
+        class="ag-theme-alpine"
+        :columnDefs="columnDefs"
+        :rowData="rowData">
+    </ag-grid-vue>
+  
+</template>
+
+<script>
+
+import { AgGridVue } from "ag-grid-vue3";
+import "ag-grid-community/styles/ag-grid.css"; 
+import "ag-grid-community/styles/ag-theme-alpine.css"; 
+
+export default {
+    components: {
+        AgGridVue
+    },
+    data(){
+        return{
+            columnDefs: [
+                {sortable: true, filter:true, headerName: "Company", field: 'company'},
+                {headerName: "Model", field: 'model'},
+                {headerName: "Price", field: 'price'},
+            ],
+            rowData: [
+                {company: 'Toyota', model: 'Celica', price: '35000'},
+                {company: 'Ford', model: 'Mondeo', price: '32000'},
+                {company: 'Porsche', model: 'Boxter', price: '72000'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
